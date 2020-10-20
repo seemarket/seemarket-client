8371baa7dda85268a98fce5801bcf006b4b16ee5
sesang06 <sesang06@naver.com>
2020-10-20 13:43:10 +0900

    main_ui

---
+++ Assets/test/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    static WebCamTexture backcam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(backcam == null) {
+            backcam = new WebCamTexture();
+        }
+
+        GetComponent<Renderer>().material.mainTexture = backcam;
+
+        if(!backcam.isPlaying){
+            backcam.Play();
+        }
+    }
+}
