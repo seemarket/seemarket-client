654985c2fdefad17a1817bf3110b8e1657804dcc
sesang06 <sesang06@naver.com>
2020-11-03 21:34:32 +0900

    Drink 클릭 프리펩 작성

--- Assets/Scripts/Singleton/CObjectPool.cs
+++ Assets/Scripts/Singleton/CObjectPool.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DrinkDetailCanvas;
 using UnityEngine;
 
 public class CObjectPool : CSingletonMono<CObjectPool>
@@ -9,11 +10,22 @@ public class CObjectPool : CSingletonMono<CObjectPool>
     DrinkObject drinkPrefab;
     Queue<DrinkObject> objectPool = new Queue<DrinkObject>();
     readonly int drinkPool = 192;
+    private DrinkDetailCanvasControl _detailCanvasControl;
     void Awake()
     {
         // Drink Prefab
         if (drinkPrefab == null)
+        {
             drinkPrefab = Resources.Load<DrinkObject>("DrinkPrefab");
+         
+        }
+       
+        if (_detailCanvasControl == null)
+        {
+            _detailCanvasControl = Instantiate(Resources.Load<DrinkDetailCanvasControl>("DrinkCanvas"));
+            _detailCanvasControl.gameObject.SetActive(false); 
+    
+        }
 
         for (int i = 0; i < drinkPool; ++i)
         {
@@ -27,7 +39,16 @@ public class CObjectPool : CSingletonMono<CObjectPool>
         prefab_collection = Resources.Load<PrefabCollection>(collection_path);
     }
 
-
+    public DrinkDetailCanvasControl CreateDetailCanvasControl(Model.Drink drink_data)
+    {
+        _detailCanvasControl.gameObject.SetActive(true);
+        _detailCanvasControl.setDrink(drink_data);
+        _detailCanvasControl.SetCanvasState();
+        return _detailCanvasControl;
+    }
+    
+    
+    
     public DrinkObject CreateDrinkObject(Model.Drink drink_data)
     {
         var go = objectPool.Dequeue();
