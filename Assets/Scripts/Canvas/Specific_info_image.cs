267387738f006deb8978b696b061f4c934fad9c7
junepaykim <junepay@snu.ac.kr>
2020-11-03 19:28:23 +0900

    texture files added but name and db should be changed

---
+++ Assets/Scripts/Canvas/Specific_info_image.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+public Button pb;
+public Sprite newSprite;
+
+public class Specific_info_image : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        pb.image.sprite = newSprite;
+    }
+
+    void Update()
+    {
+        
+    }
+}
